472aec94e20a9b7662aaca8b96dd3d90e1481999
p0x0q <p0x0q_jp@gmail.com>
2020-11-12 01:04:58 +0900

    Create pso2_log_search.py

---
+++ pso2_log_search.py
@@ -0,0 +1,17 @@
+import os
+
+os.chdir(os.getenv("HOMEDRIVE") + os.getenv("HOMEPATH") + "\\Documents" + r"\SEGA\PHANTASYSTARONLINE2\log")
+
+import glob
+
+search = input("検索したい文字列を入力：")
+for filepath in glob.glob("./*"):
+    print(filepath)
+    try:
+        fr = open(filepath, "r",encoding="utf-16")
+        for text in fr.readlines():
+            if search in text:
+                print(text)
+    except:
+        pass
+input("")
